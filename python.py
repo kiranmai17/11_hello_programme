05696323e5b243737d304e571a2b3fbb991681ac
kiranmai17 <116304133+kiranmai17@users.noreply.github.com>
2022-10-30 21:50:21 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+'''
+DATE=30th oct 2022
+DAY= sunday
+TOPIC= hell0 program 
+AUTHOR= KIRANMAI
+'''
+print("hello") #hello
+print("kiranmai") #kiranmai
+print("enjoy the weekend") #enjoy the weekend
+
